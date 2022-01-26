7508f05175ae438059647d9e5793f5f72f9f30f5
zlw-qa <asdori95@gmail.com>
2022-01-27 01:16:35 +0400

    improve gradle

--- src/test/java/com/github/zlwqa/tests/LocalTests.java
+++ src/test/java/com/github/zlwqa/tests/LocalTests.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 @Tag("local")
